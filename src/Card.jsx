a384f7cda7c199e3bc1176545b7baccd444393b0
kcrusselluniversity <kcrussell.university@gmail.com>
2023-10-13 13:57:13 +1100

    Add tilt effect on cards

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,5 +1,6 @@
-// TODO: Add an onLoad prop to the img tag and have a count in the App that 
-// shows loading until all X cards have been fully loaded
+// TODO: USE FLIP-HORIZONTALLY PROP of TILT to have a nice animation
+// when the card flips over
+import Tilt from 'react-parallax-tilt';
 
 export default function Card({ 
     name, imgUrl, disabled, visible, id, 
@@ -27,11 +28,13 @@ export default function Card({
     }
 
     return (
-        <div className="card" onClick={handleClick}>
-            <div className={visible ? null : 'hidden'}>
-                <h3 style={{margin: '0px', paddingBottom: '6px', color: 'black'}}>{name}</h3>
-                <img onLoad={() => {setLoadedImageCount(loadedImageCount => loadedImageCount + 1)}}src={imgUrl} style={{height: '150px', width: '150px'}}/>
+        <Tilt tiltEnable={!disabled} transitionSpeed={600} perspective={1000} scale={1.02}>
+            <div className="card" onClick={handleClick}>
+                <div className={visible ? null : 'hidden'}>
+                    <h3 style={{margin: '0px', paddingBottom: '6px', color: 'black'}}>{name}</h3>
+                    <img onLoad={() => {setLoadedImageCount(loadedImageCount => loadedImageCount + 1)}}src={imgUrl} style={{height: '150px', width: '150px'}}/>
+                </div>
             </div>
-        </div>
+        </Tilt>
     )
 }
